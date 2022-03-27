f522bf2f3210940496ca2044fe8740cbf1f0d3e5
Hajime Hatano <hajime9652@gmail.com>
2022-03-27 14:57:03 +0900

    add page, layout and component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,38 +1,16 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
+  mode: "jit",
   content: [
-    "./components/**/*.{js,vue,ts}",
+    "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
   ],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    colors: {
-      white: colors.white,
-      'sws': {
-        main1: '#36b495',
-        main2: '#e98787',
-        accent1: '#e1e16c',
-        accent2: '#80b5da',
-        accent3: '#a295c6',
-        gray1: '#f4f8fc',
-        gray2: '#b5bdcb',
-        gray3: '#5f5f5f',
-        gray4: '#414141',
-        success: '#36b495',
-        error: '#ea493d',
-        alert: '#eac736',
-        normal: '#f4f8fc',
-      },
-    },
-    extend: {
-      colors: {
-        rose: colors.rose,
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
